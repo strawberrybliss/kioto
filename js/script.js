c8110511e74521e0be60f1e95c08c702816cdb2a
strawberrybliss <lucisafl78@gmail.com>
2025-11-27 15:48:11 +0100

    cambios 27/11 (2)

--- js/script.js
+++ js/script.js
@@ -25,3 +25,58 @@ function closeModal() {
 
   return false;
 }
+
+// gráfico de barras
+
+function cargaGraficoBarras() {
+    // Datos para generar el gráfico, hay que definir las etiquetas y
+    // los datasets. Hay que definir un color para cada dataset
+    var datos = {
+        labels: ["Kinkaku-ji", "Fushimi Inari-taisha", "Ginkaku-ji", "Kiyomizu-dera"],
+        datasets: [{
+            label: "Templo más visitado anualmente",
+            backgroundColor: "#f2a6abff",
+            data: [44.1, 21.4, 16.7, 6.2]
+        }],
+    };
+
+    // Configuración del gráfico. Debe incluir imprescindiblemente el
+    // tipo de gráfico y los datos que hemos definido previamente.
+    // Se pueden añadir opciones para personalizar el gráfico
+    var config = {
+        type: "bar",
+        data: datos,
+        options: {
+            scales: {
+                x: {
+                    border: {
+                        color: "black",
+                        width: 2
+                    },
+                    grid: {
+                        display: false
+                    }
+                },
+                y: {
+                    min: 0,
+                    max: 50,
+                    title: {
+                        display: true,
+                        text: "Porcentaje de visitantes en 2025 (%)"
+                    },
+                    border: {
+                        color: "black",
+                        width: 2
+                    }
+                }
+            }
+        }
+    };
+
+    // Para crear el gráfico, se busca el elemento canvas por su id
+    // y se le pasa la configuración en JSON que hemos definido
+    var grafico = $("#barras")[0].getContext("2d");
+    new Chart(grafico, config);
+}
+
+// carrusel imagenes
